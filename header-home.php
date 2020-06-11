8d6e9039148c8770a79e82a9328e64740c783ae7
JeremyJamesL <47485563+JeremyJamesL@users.noreply.github.com>
2020-06-11 14:44:14 +0100

    Added partials/background-video for headers

--- header-home.php
+++ header-home.php
@@ -24,41 +24,7 @@
 <!-- Background video
 ======================= -->
 
-<video autoplay muted loop class="background-video" id="video"> <!-- Don't forget to add back autoplay -->
-    <source src="<?php if(get_theme_mod('wa_customise_background_video_webm')) {  
-
-            echo get_theme_mod('wa_customise_background_video_webm');
-
-            } else {
-                echo get_template_directory_uri() . '/assets/video/WaxAxes.webm';
-            }
-
-          ?>" type="video/webm">
-
-    <source src="<?php if(get_theme_mod('wa_customise_background_video_mp4')) {  
-
-            echo get_theme_mod('wa_customise_background_video_mp4');
-
-            } else {
-                echo get_template_directory_uri() . '/assets/video/WaxAxes.mp4';
-            }
-
-    ?>" type="video/mp4">
-
-    <p>Your browser doesn't support the HTML5 video. Consider upgrading to a more modern browser.</p>
-
-</video><!-- #background video end -->
-
-
-<!-- Background video controls
-========================== -->
-
-<!-- <div id="video-controls">
-    <button type="button" class="play-pause" id="play-pause">+</button>
-</div> -->
-
-<!-- #End background video controls -->
-
+<?php get_template_part( '/partials/background-video' ); ?>
 
 
 <!-- Primary Navigation
--- header.php
+++ header.php
@@ -30,30 +30,8 @@
     echo ''; // Don't autoplay on check and basket pages
 } else {  ?>
 
-<video autoplay muted loop class="background-video" id="video">
-    <source src="<?php if(get_theme_mod('wa_customise_background_video_webm')) {  
+<?php get_template_part( '/partials/background-video' ); ?>
 
-            echo get_theme_mod('wa_customise_background_video_webm');
-
-            } else {
-                echo get_template_directory_uri() . '/assets/video/WaxAxes.webm';
-            }
-
-          ?>" type="video/webm">
-
-    <source src="<?php if(get_theme_mod('wa_customise_background_video_mp4')) {  
-
-            echo get_theme_mod('wa_customise_background_video_mp4');
-
-            } else {
-                echo get_template_directory_uri() . '/assets/video/WaxAxes.mp4';
-            }
-
-    ?>" type="video/mp4">
-
-    <p>Your browser doesn't support the HTML5 video. Consider upgrading to a more modern browser.</p>
-
-</video><!-- #background video end -->
 
 <?php } ?>
 
